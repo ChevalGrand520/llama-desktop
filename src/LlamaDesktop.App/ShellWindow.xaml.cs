9e786ec02bc3dc150261b110912c524d8f5c61f4
Llama Desktop Builder <builder@local>
2026-08-28 17:08:09 +0800

    fix: wire log pane to view model

--- src/LlamaDesktop.App/ShellWindow.xaml.cs
+++ src/LlamaDesktop.App/ShellWindow.xaml.cs
@@ -1,6 +1,7 @@
+using System.Collections.Specialized;
 using System.Windows;
-using Microsoft.Web.WebView2.Wpf;
 using LlamaDesktop.App.Presentation.ViewModels;
+using Microsoft.Web.WebView2.Wpf;
 
 namespace LlamaDesktop.App;
 
@@ -8,11 +9,28 @@ public partial class ShellWindow : Window
 {
     private readonly WebView2 _webView;
 
-    public ShellWindow(ShellViewModel viewModel, Microsoft.Web.WebView2.Wpf.WebView2 webView)
+    public ShellWindow(ShellViewModel viewModel, WebView2 webView)
     {
         InitializeComponent();
         DataContext = viewModel;
         _webView = webView;
         WebHostGrid.Children.Add(webView);
+        viewModel.Log.Lines.CollectionChanged += OnLogLinesChanged;
+    }
+
+    private void OnLogLinesChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        // ProcessExited can raise on a threadpool thread; marshal to the UI dispatcher.
+        Dispatcher.BeginInvoke(new Action(() =>
+        {
+            if (e.Action == NotifyCollectionChangedAction.Add && e.NewItems is not null)
+            {
+                foreach (var item in e.NewItems)
+                {
+                    LogText.AppendText($"{item}\r\n");
+                }
+                LogText.ScrollToEnd();
+            }
+        }));
     }
 }
